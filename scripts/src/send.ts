26a55e8b7e61ea5410c210d2ca2f0c3303da6d3a
Thounyy <thouny@tuta.io>
2024-05-06 11:46:00 +0200

    feat: remove multisig
    will use the external sui-multisig we're building

--- scripts/src/send.ts
+++ scripts/src/send.ts
@@ -7,9 +7,9 @@ import { client, keypair, getId } from './utils.js';
 
 		const tx = new TransactionBlock();
 
-		const [coin] = tx.splitCoins(tx.object("coinId"), [10]);
+		const [coin] = tx.splitCoins(tx.object("0x4a3f6a03bc26a2f883452029c81174e63858e74924235008074b6bbd196b8bbf"), [1000]);
 
-		tx.transferObjects([coin], "0xdc2dbdf749bcf228a97339020607110baf45248ccc3e7671edd3e3c866a75717");
+		tx.transferObjects([coin], "0xa0cd8ac1269f658a75a13b15c97743e0ba3b67ec6107bcf87dc2ec8466170616");
 
 		tx.setGasBudget(10000000);
 
